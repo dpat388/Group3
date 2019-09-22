3344b0c5bfe55551b70b7810a8e0d6ec73cd6637
dpat388 <55531012+dpat388@users.noreply.github.com>
2019-09-22 22:26:34 +1200

    Version2

---
+++ Version1/Assets/Scenes/ChangeScene.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public void GotoMainScene()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void GotoSpaceScene()
+    {
+        SceneManager.LoadScene("SpaceScene");
+    }
+}
+
