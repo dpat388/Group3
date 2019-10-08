7850f5473e1dcc7c3e07db1197aa9caa6e9a19b9
dpat388 <55531012+dpat388@users.noreply.github.com>
2019-10-08 17:41:00 +1300

    Version3

---
+++ Version1/Assets/Scenes/Ui.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ui : MonoBehaviour
+{
+	[SerializeField] private Image customImage;
+	void onTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			customImage.enabled = true;
+		}
+	}
+
+	void onTriggerExit(Collider other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			customImage.enabled = false;
+		}
+	}
+}
